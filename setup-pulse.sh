a69a0fe6f4eb8ea0553da526e6f18f1c35ff3f62
Raymond Cheng <me@raymondcheng.net>
2017-02-25 16:20:14 -0800

    fix pulse script

--- setup-pulse.sh
+++ setup-pulse.sh
@@ -2,24 +2,34 @@
 
 sudo apt-get install pulseaudio pulseaudio-utils alsa-utils avahi-daemon pavucontrol
 
-temp=`cat /etc/modules | grep snd-bcm2835`
-if [ -z $temp ]; then
-	echo "adding snd-bcm2835 to /etc/modules"
+if cat /etc/modules | grep "snd-bcm2835" | grep -v "#" >/dev/null; then echo "### sound driver configured for boot"
+else
+	echo "### adding snd-bcm2835 to /etc/modules"
 	sudo echo "snd-bcm2835" | sudo tee -a /etc/modules
-	echo "now reboot the system"
+	echo "NOW REBOOT SYSTEM"
 	exit 1
 fi
 
-temp=`lsmod| grep snd_bcm2835`
-if [ -z $temp ]; then
-	echo "snd-bcm2835 kernel module not loaded. Please fix before continuing"
+if lsmod | grep "snd_bcm2835" >/dev/null; then echo "### sound driver loaded"
+else
+	echo "### snd-bcm2835 kernel module not loaded. Please fix before continuing"
 	exit 1
+fi
+
+if cat /etc/pulse/default.pa | grep "echo-cancel" | grep -v "#" >/dev/null; then echo "### echo cancellation installed"
+else
+	echo "### adding module-echo-cancel to /etc/pulse/default.pa "
+	sudo echo "load-module module-echo-cancel" | sudo tee -a /etc/pulse/default.pa
 fi
 
 # 0=auto, 1=headphones, 2=hdmi
 sudo amixer -c 0 cset numid=3 0
 
 echo "Starting PulseAudio"
-pulseaudio -D --high-priority --disallow-exit --disallow-module-loading=1
+pulseaudio -D --high-priority #--disallow-exit --disallow-module-loading=1
+
+if pactl list | grep "echo-cancel" >/dev/null; then echo "### echo cancellation running"
+else echo "### echo cancellation not running for some reason"
+fi
 
 # `pavucontrol` for pulseaudio GUI
